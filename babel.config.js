2878fb7eb60547a414dd8c149c1b5fce22db8eb5
Omar Qaddoumi <omarqaddoumi@gmail.com>
2021-09-24 23:21:17 -0700

    style(linting): fix files linting

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    presets: [
-      [
-        "@babel/preset-env",
-        {
-          targets: {
-            node: "14",
-          },
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          node: "14",
         },
-      ],
-      "@babel/preset-typescript",
+      },
     ],
-  };
+    "@babel/preset-typescript",
+  ],
+};
--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    transform: {
-      "^.+\\.[t|j]sx?$": "babel-jest",
-    },
-    testRegex: "(/__tests__/.*(\\.|/)(test|spec))\\.tsx?$",
-    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-    coverageDirectory: "coverage",
-  };
+  transform: {
+    "^.+\\.[t|j]sx?$": "babel-jest",
+  },
+  testRegex: "(/__tests__/.*(\\.|/)(test|spec))\\.tsx?$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  coverageDirectory: "coverage",
+};
