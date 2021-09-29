8999a58ba0ac4a82d464829f8213e0b6efddea8e
Ahmadreza Tabibi <ahmadreza.tabibi@gmail.com>
2021-09-28 17:45:30 -0700

    Revert "Solution/#3 palindrome premutation"

--- src/__tests__/palindromePremutation.test.ts
+++
@@ -1,9 +0,0 @@
-import { isPermutePalindrome } from "../palindromePremutation";
-
-test("should return false for 'code' as it is not PalindromePremutation", () => {
-  expect(isPermutePalindrome("code")).toBe(false);
-});
-
-test("should return true for 'aab' as it is PalindromePremutation", () => {
-  expect(isPermutePalindrome("aab")).toBe(true);
-});
